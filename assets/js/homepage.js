5f0a35ab87b119907e685216ecf6e60d10a4331a
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-24 22:28:04 -0800

    added function to add parameter for fetch URL

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-
-getUserRepos();
+var getUserRepos = function (user) {
+    const apiURL = `https://api.github.com/users/${user}/repos`;
+    fetch(apiURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+        })
+};
+getUserRepos("abarragan89");
